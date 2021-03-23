ce15ae5ae82e897e0adde3857d4b756bf12e789c
LisaHerbig <72883072+LisaHerbig@users.noreply.github.com>
2021-03-23 21:51:05 +0100

    herumprobieren in der Konsole, Aufgaben aus dem Inverted classroom

---
+++ L01_RandomPoem/Recap.js
@@ -0,0 +1,7 @@
+"use strict";
+let v = 1;
+v = v + 1;
+console.log(v);
+let a = [7, true, "Hallo"];
+console.log(a);
+//# sourceMappingURL=Recap.js.map
---
+++ L01_RandomPoem/Recap.ts
@@ -0,0 +1,9 @@
+let v: number = 1;
+v = v + 1;
+console.log(v);
+let a = [7, true, "Hallo"];
+console.log(a);
+
+
+
+
