f2f6c02dfc3becc6f945588c3eb5584b87981e61
LisaHerbig <72883072+LisaHerbig@users.noreply.github.com>
2021-03-23 22:39:35 +0100

    zu Aufgabe 1

--- L01_RandomPoem/L01_RandomPoem.js
+++ L01_RandomPoem/L01_RandomPoem.js
@@ -1,3 +1,11 @@
 "use strict";
-console.log("Expecto Patronum");
+var L01Poem;
+(function (L01Poem) {
+    let subjekt = ["Hermine", "Harry", "Ron", "Hagrid", "Dobby", "Der sprechende Hut"];
+    let praedikat = ["stiehlt", "verzaubert", "isst", "verflucht", "liebt", "belehrt"];
+    let objekt = ["Schlafbohnen", "Drachen", "Dumbledores Phönix", "den Schnatz", "Krummbein", "die Heiligtümer des Todes"];
+    for (let i = 6; i >= subjekt.length; i--) {
+        console.log("Lumos" + i);
+    }
+})(L01Poem || (L01Poem = {}));
 //# sourceMappingURL=L01_RandomPoem.js.map
--- L01_RandomPoem/L01_RandomPoem.ts
+++ L01_RandomPoem/L01_RandomPoem.ts
@@ -1,1 +1,12 @@
-console.log("Expecto Patronum");
+namespace L01Poem {
+    let subjekt: string [] = ["Hermine", "Harry", "Ron", "Hagrid", "Dobby", "Der sprechende Hut"];
+    let praedikat: string [] = ["stiehlt", "verzaubert", "isst", "verflucht", "liebt", "belehrt"];
+    let objekt: string [] = ["Schlafbohnen", "Drachen", "Dumbledores Phönix", "den Schnatz", "Krummbein", "die Heiligtümer des Todes"];
+    
+    for (let i: number = 6; i >= subjekt.length; i --) {
+        console.log("Lumos" + i);
+        
+    }
+    
+    
+}
