beb40580e1b7a2b3a3fa32d26744f18c60066f63
LisaHerbig <72883072+LisaHerbig@users.noreply.github.com>
2021-03-23 22:03:16 +0100

    aufgeräumt

--- L01_RandomPoem/Recap.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let v = 1;
-v = v + 1;
-console.log(v);
-let a = [7, true, "Hallo"];
-console.log(a);
-//# sourceMappingURL=Recap.js.map
--- L01_RandomPoem/Recap.ts
+++
@@ -1,9 +0,0 @@
-let v: number = 1;
-v = v + 1;
-console.log(v);
-let a = [7, true, "Hallo"];
-console.log(a);
-
-
-
-
