c76eab310854b03cc06b986502224a5cbce99c8f
LisaHerbig <72883072+LisaHerbig@users.noreply.github.com>
2021-03-30 10:38:32 +0200

    Blackmailer

---
+++ L02/test.ts
@@ -0,0 +1,43 @@
+namespace L02_BlackmailerCompanion {
+let chosenCharacter: string = "A";
+window.addEventListener("load", handleLoad);
+
+function handleLoad(_event: Event): void{
+    let mail: HTMLElement = <HTMLElement> document.querySelector("div#mail");
+    mail.addEventListener("click", placeLetter);
+    document.addEventListener("keydown", chooseCharacter);
+}
+
+function placeLetter(_event: MouseEvent): void {
+     if (_event.target == _event.currentTarget) {
+    // console.log(_event);
+    let x: number = _event.offsetX;
+    let y: number = _event.offsetY;
+
+    let mail: HTMLElement = <HTMLElement>_event.target;
+    let letter: HTMLSpanElement = document.createElement("span");
+    mail.appendChild(letter);
+
+    letter.textContent = chosenCharacter;
+    letter.style.left = x + "px";
+    letter.style.top = y + "px";
+     } else {
+         deleteLetter(_event);
+     }    
+    // letter.addEventListener("click", deleteLetter);
+    // }
+
+    
+}
+function chooseCharacter(_event: KeyboardEvent): void {
+    // console.log(_event);
+    chosenCharacter = _event.key;
+}
+
+function deleteLetter(_event: MouseEvent): void {
+    // _event.stopPropagation();
+    let target: Node = <Node>_event.target;
+    let parent: Node = <Node>target.parentNode;
+    parent.removeChild(target); 
+}
+}
