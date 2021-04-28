b6a4e728cd839f009269fcd23f19021c678b4e73
LisaHerbig <72883072+LisaHerbig@users.noreply.github.com>
2021-04-28 21:26:24 +0200

    Alle möglichen Fronts deklariert

--- L03_MemorySettings/scriptMemory.ts
+++ L03_MemorySettings/scriptMemory.ts
@@ -42,6 +42,283 @@ let fronts: Front[] = [
         font: fontStyle,
         color: fontColor,
         text: "Mouse"
+    },
+    {
+        class: "pair3",
+        font: fontStyle,
+        color: fontColor,
+        text: "Hase"
+    },
+    {
+        class: "pair3",
+        font: fontStyle,
+        color: fontColor,
+        text: "rabbit"
+    },
+    {
+        class: "pair4",
+        font: fontStyle,
+        color: fontColor,
+        text: "Löwe"
+    },
+    {
+        class: "pair4",
+        font: fontStyle,
+        color: fontColor,
+        text: "Lion"
+
+    },
+    {
+        class: "pair5",
+        font: fontStyle,
+        color: fontColor,
+        text: "Katze"
+    },
+    {
+        class: "pair5",
+        font: fontStyle,
+        color: fontColor,
+        text: "Cat"
+    },
+    {
+        class: "pair6",
+        font: fontStyle,
+        color: fontColor,
+        text: "Hund"
+    },
+    {
+        class: "pair6",
+        font: fontStyle,
+        color: fontColor,
+        text: "Dog"
+    },
+    {
+        class: "pair7",
+        font: fontStyle,
+        color: fontColor,
+        text: "Fisch"
+    },
+    {
+        class: "pair7",
+        font: fontStyle,
+        color: fontColor,
+        text: "Fish"
+    },
+    {
+        class: "pair8",
+        font: fontStyle,
+        color: fontColor,
+        text: "Frosch"
+    },
+    {
+        class: "pair8",
+        font: fontStyle,
+        color: fontColor,
+        text: "Frog"
+    },
+    {
+        class: "pair9",
+        font: fontStyle,
+        color: fontColor,
+        text: "Elefant"
+    },
+    {
+        class: "pair9",
+        font: fontStyle,
+        color: fontColor,
+        text: "elephant"
+    },
+    {
+        class: "pair10",
+        font: fontStyle,
+        color: fontColor,
+        text: "Schlange"
+    },
+    {
+        class: "pair10",
+        font: fontStyle,
+        color: fontColor,
+        text: "Snake"
+    },
+    {
+        class: "pair11",
+        font: fontStyle,
+        color: fontColor,
+        text: "Spinne"
+    },
+    {
+        class: "pair11",
+        font: fontStyle,
+        color: fontColor,
+        text: "Spider"
+    },
+    {
+        class: "pair12",
+        font: fontStyle,
+        color: fontColor,
+        text: "Biene"
+    },
+    {
+        class: "pair12",
+        font: fontStyle,
+        color: fontColor,
+        text: "Bee"
+    },
+    {
+        class: "pair13",
+        font: fontStyle,
+        color: fontColor,
+        text: "Hummel"
+    },
+    {
+        class: "pair13",
+        font: fontStyle,
+        color: fontColor,
+        text: "Bumblebee"
+    },
+    {
+        class: "pair14",
+        font: fontStyle,
+        color: fontColor,
+        text: "Eidechse"
+    },
+    {
+        class: "pair14",
+        font: fontStyle,
+        color: fontColor,
+        text: "Lizard"
+    },
+    {
+        class: "pair15",
+        font: fontStyle,
+        color: fontColor,
+        text: "Krokodil"
+    },
+    {
+        class: "pair15",
+        font: fontStyle,
+        color: fontColor,
+        text: "Crocodile"
+    },
+    {
+        class: "pair16",
+        font: fontStyle,
+        color: fontColor,
+        text: "Biber"
+    },
+    {
+        class: "pair16",
+        font: fontStyle,
+        color: fontColor,
+        text: "Beaver"
+    },
+    {
+        class: "pair17",
+        font: fontStyle,
+        color: fontColor,
+        text: "Dachs"
+    },
+    {
+        class: "pair17",
+        font: fontStyle,
+        color: fontColor,
+        text: "Badger"
+    },
+    {
+        class: "pair18",
+        font: fontStyle,
+        color: fontColor,
+        text: "Delfin"
+    },
+    {
+        class: "pair18",
+        font: fontStyle,
+        color: fontColor,
+        text: "Dolphin"
+    },
+    {
+        class: "pair19",
+        font: fontStyle,
+        color: fontColor,
+        text: "Eichhörnchen"
+    },
+    {
+        class: "pair19",
+        font: fontStyle,
+        color: fontColor,
+        text: "Squirrel"
+    },
+    {
+        class: "pair20",
+        font: fontStyle,
+        color: fontColor,
+        text: "Fuchs"
+    },
+    {
+        class: "pair20",
+        font: fontStyle,
+        color: fontColor,
+        text: "Fox"
+    },
+    {
+        class: "pair21",
+        font: fontStyle,
+        color: fontColor,
+        text: "Igel"
+    },
+    {
+        class: "pair21",
+        font: fontStyle,
+        color: fontColor,
+        text: "Hedgehog"
+    },
+    {
+        class: "pair22",
+        font: fontStyle,
+        color: fontColor,
+        text: "Maulwurf"
+    },
+    {
+        class: "pair22",
+        font: fontStyle,
+        color: fontColor,
+        text: "Mole"
+    },
+    {
+        class: "pair23",
+        font: fontStyle,
+        color: fontColor,
+        text: "Schaf"
+    },
+    {
+        class: "pair23",
+        font: fontStyle,
+        color: fontColor,
+        text: "Sheep"
+    },
+    {
+        class: "pair24",
+        font: fontStyle,
+        color: fontColor,
+        text: "Pferd"
+    },
+    {
+        class: "pair24",
+        font: fontStyle,
+        color: fontColor,
+        text: "Horse"
+    },
+    {
+        class: "pair25",
+        font: fontStyle,
+        color: fontColor,
+        text: "Schmetterling"
+    },
+    {
+        class: "pair25",
+        font: fontStyle,
+        color: fontColor,
+        text: "Butterfly"
     }
 ];
 
