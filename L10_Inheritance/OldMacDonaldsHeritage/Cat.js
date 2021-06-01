211cbd1f08de67c4aea9b4a202cf6c39f5d5b31e
LisaHerbig <72883072+LisaHerbig@users.noreply.github.com>
2021-06-01 18:14:50 +0200

    cat fertig

--- L10_Inheritance/OldMacDonaldsHeritage/Cat.js
+++ L10_Inheritance/OldMacDonaldsHeritage/Cat.js
@@ -18,6 +18,39 @@ var L10_1_OldMacDonalsHeritage;
                 "Everywhere a " + this.sound + "" + this.sound + "\n" +
                 "Old Mac Donald had a farm " + "\n" +
                 "Ee i ee i o");
+            //On Canvas
+            L10_1_OldMacDonalsHeritage.crc2d.save();
+            L10_1_OldMacDonalsHeritage.crc2d.font = "12px Arial";
+            L10_1_OldMacDonalsHeritage.crc2d.strokeText("Old Mac Donald had a farm", 665, 10);
+            L10_1_OldMacDonalsHeritage.crc2d.restore();
+            L10_1_OldMacDonalsHeritage.crc2d.save();
+            L10_1_OldMacDonalsHeritage.crc2d.font = "12px Arial";
+            L10_1_OldMacDonalsHeritage.crc2d.strokeText("Ee i ee i o", 665, 25);
+            L10_1_OldMacDonalsHeritage.crc2d.restore();
+            L10_1_OldMacDonalsHeritage.crc2d.save();
+            L10_1_OldMacDonalsHeritage.crc2d.font = "12px Arial";
+            L10_1_OldMacDonalsHeritage.crc2d.strokeText("And on his farm he had some " + this.species + "s", 665, 40);
+            L10_1_OldMacDonalsHeritage.crc2d.restore();
+            L10_1_OldMacDonalsHeritage.crc2d.save();
+            L10_1_OldMacDonalsHeritage.crc2d.font = "12px Arial";
+            L10_1_OldMacDonalsHeritage.crc2d.strokeText("Ee i ee i o", 665, 55);
+            L10_1_OldMacDonalsHeritage.crc2d.restore();
+            L10_1_OldMacDonalsHeritage.crc2d.save();
+            L10_1_OldMacDonalsHeritage.crc2d.font = "12px Arial";
+            L10_1_OldMacDonalsHeritage.crc2d.strokeText("With a " + this.sound + " " + this.sound + " here and a " + this.sound + " " + this.sound + " there", 665, 70);
+            L10_1_OldMacDonalsHeritage.crc2d.restore();
+            L10_1_OldMacDonalsHeritage.crc2d.save();
+            L10_1_OldMacDonalsHeritage.crc2d.font = "12px Arial";
+            L10_1_OldMacDonalsHeritage.crc2d.strokeText("Here a " + this.sound + " there a " + this.sound + "Everywhere a " + this.sound + "" + this.sound, 665, 85);
+            L10_1_OldMacDonalsHeritage.crc2d.restore();
+            L10_1_OldMacDonalsHeritage.crc2d.save();
+            L10_1_OldMacDonalsHeritage.crc2d.font = "12px Arial";
+            L10_1_OldMacDonalsHeritage.crc2d.strokeText("Old Mac Donald had a farm ", 665, 100);
+            L10_1_OldMacDonalsHeritage.crc2d.restore();
+            L10_1_OldMacDonalsHeritage.crc2d.save();
+            L10_1_OldMacDonalsHeritage.crc2d.font = "12px Arial";
+            L10_1_OldMacDonalsHeritage.crc2d.strokeText("Ee i ee i o", 665, 115);
+            L10_1_OldMacDonalsHeritage.crc2d.restore();
         }
         eaten() {
             let leftOverFish = 0;
@@ -25,9 +58,19 @@ var L10_1_OldMacDonalsHeritage;
             L10_1_OldMacDonalsHeritage.totalAmountFish = leftOverFish;
             console.log("Hello, my Name is " + this.myName + ". I am a " + this.species + " and I ate " + this.ate + " kg of " + this.favFood + " today. That means " + leftOverFish + " kg are left.");
             //leftOver = 0;
+            //Canvas
+            L10_1_OldMacDonalsHeritage.crc2d.save();
+            L10_1_OldMacDonalsHeritage.crc2d.font = "12px Arial";
+            L10_1_OldMacDonalsHeritage.crc2d.strokeText("Hello, my Name is " + this.myName + ". I am a " + this.species + " and I ate " + this.ate + " kg of " + this.favFood + " today. That means " + leftOverFish + " kg are left.", 600, 135);
+            L10_1_OldMacDonalsHeritage.crc2d.restore();
         }
         doSpecialAction() {
             console.log("My special action is  " + this.special);
+            //Canvas
+            L10_1_OldMacDonalsHeritage.crc2d.save();
+            L10_1_OldMacDonalsHeritage.crc2d.font = "12px Arial";
+            L10_1_OldMacDonalsHeritage.crc2d.strokeText("My special Action is " + this.special, 600, 155);
+            L10_1_OldMacDonalsHeritage.crc2d.restore();
         }
         draw() {
             L10_1_OldMacDonalsHeritage.drawCat();
